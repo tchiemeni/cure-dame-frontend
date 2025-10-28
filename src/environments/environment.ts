9900d9e444f054d5271a91128b42296f561cf594
tchiemeni <ornellatchiemeni@gmail.com>
2025-10-28 09:37:13 +0000

    evironments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Assurez-vous que c'est l'URL où votre serveur Laravel tourne (php artisan serve)
-  apiUrl: 'https://cure-dame-api.onrender.com',
+  apiUrl: 'https://cure-dame-api-production.up.railway.app/api',
 };
